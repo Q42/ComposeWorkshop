971c26a66e71c43b34196eb65b11ce0089ddc831
Frank1234 <frankf@q42.nl>
2025-05-21 12:11:34 +0200

    CHANGE edge to edge fix for Android 16

--- app/src/main/kotlin/nl/q42/instagram/ui/MainActivity.kt
+++ app/src/main/kotlin/nl/q42/instagram/ui/MainActivity.kt
@@ -3,8 +3,11 @@ package nl.q42.instagram.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import nl.q42.instagram.ui.data.dummyViewState
@@ -18,7 +21,13 @@ class MainActivity : ComponentActivity() {
             AppTheme {
 
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Home(dummyViewState)
+                    Scaffold { contentPadding ->
+                        Box(Modifier
+                            .fillMaxSize()
+                            .padding(contentPadding.calculateTopPadding())) {
+                            Home(dummyViewState)
+                        }
+                    }
                 }
             }
         }
